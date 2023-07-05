6249a924945032f4c73ddee211c18af6daa71a8a
TLRKiliann <philogenie@protonmail.com>
2023-07-05 20:28:08 +0200

    some changes

--- my-app/components/navbar.tsx
+++ my-app/components/navbar.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import style from '@/styles/navbar.module.scss'
 
 export default function Navbar() {
   return (
     <>
-      <nav style={{color: "white"}}>NavBar</nav>
+      <nav class={style.navbar}>
+        <h2 class={style.image}>Portfolio</h2>
+        <ul class={style.ul}>
+          <li class={style.li}><Link href="/navbar" class={style.link}>Skills</Link></li>
+          <li class={style.li}><Link href="/navbar" class={style.link}>Projects</Link></li>
+          <li class={style.li}><Link href="/navbar" class={style.link}>Contact</Link></li>
+        </ul>
+      </nav>
     </>
   )
 }
