a2c9926da306cdb7349b3b4185a112faf0a82597
TLRKiliann <philogenie@protonmail.com>
2023-07-08 19:08:04 +0200

    animation added

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,31 +1,41 @@
-import { Metadata } from 'next'
+'use client'
+
+import { useRef } from 'react'
+import { useInView } from 'react-intersection-observer';
 import MainTitle from '@/components/mainTitle'
 import SkillsSection from '@/components/skillsSection'
 import ProjectsSection from '@/components/projectsSection'
 import ContactSection from '@/components/contactSection'
 import style from '@/styles/page.module.scss'
-
-export const metadata: Metadata = {
-  title: 'Portfolio',
-}
  
 export default function Page() {
+
+  const { ref: myRef, inView: myElementIsVisible } = useInView();
+  const { ref: projectRef, inView: projectsElementSection } = useInView();
+  const { ref: contactRef, inView: contactElementSection } = useInView();
+
   return (
     <>
       <section className={style.sectionMainTitle}>
         <MainTitle />
       </section>
+      
       <hr className={style.hr}/>
-      <section className={style.sectionSkillsSection}>
-        <SkillsSection />
+
+      <section ref={myRef} className={style.sectionSkillsSection}>
+        {myElementIsVisible ? <SkillsSection /> : null}
       </section>
+      
       <hr className={style.hr}/>
-      <section className={style.sectionProjectsSection}>
-        <ProjectsSection />
+      
+      <section ref={projectRef} className={style.sectionProjectsSection}>
+        {projectsElementSection ? <ProjectsSection /> : null}
       </section>
+      
       <hr className={style.hr}/>
-      <section className={style.sectionContactSection}>
-        <ContactSection />
+      
+      <section ref={contactRef} className={style.sectionContactSection}>
+        {contactElementSection ? <ContactSection /> : null}
       </section>
 
     </>
