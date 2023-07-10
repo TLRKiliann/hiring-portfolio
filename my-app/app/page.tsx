949f53739a79a84cb6a621af919ec71bacc3a51e
TLRKiliann <philogenie@protonmail.com>
2023-07-10 18:24:44 +0200

    setup for framer-motion done

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,43 +1,49 @@
-'use client'
-
-import { useRef } from 'react'
-import { useInView } from 'react-intersection-observer';
+import Layout from "@/components/layout"
 import MainTitle from '@/components/mainTitle'
 import SkillsSection from '@/components/skillsSection'
 import ProjectsSection from '@/components/projectsSection'
 import ContactSection from '@/components/contactSection'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 import style from '@/styles/page.module.scss'
  
 export default function Page() {
 
-  const { ref: myRef, inView: myElementIsVisible } = useInView();
-  const { ref: projectRef, inView: projectsElementSection } = useInView();
-  const { ref: contactRef, inView: contactElementSection } = useInView();
-
   return (
     <>
-      <section className={style.sectionMainTitle}>
-        <MainTitle />
-      </section>
-      
-      <hr className={style.hr}/>
+      <Navbar />
 
-      <section ref={myRef} className={style.sectionSkillsSection}>
-        {myElementIsVisible ? <SkillsSection /> : null}
-      </section>
-      
-      <hr className={style.hr}/>
-      
-      <section ref={projectRef} className={style.sectionProjectsSection}>
-        {projectsElementSection ? <ProjectsSection /> : null}
-      </section>
-      
-      <hr className={style.hr}/>
-      
-      <section ref={contactRef} className={style.sectionContactSection}>
-        {contactElementSection ? <ContactSection /> : null}
-      </section>
+      <Layout>
+        <section className={style.sectionMainTitle}>
+          <MainTitle />
+        </section>
+      </Layout>
 
+      <hr className={style.hr} />
+
+      <Layout>
+        <section className={style.sectionSkillsSection}>
+          <SkillsSection /> 
+        </section>
+      </Layout>
+
+      <hr className={style.hr} />
+
+      <Layout>
+        <section className={style.sectionProjectsSection}>
+          <ProjectsSection />
+        </section>
+      </Layout>
+
+      <hr className={style.hr} />
+
+      <Layout>
+        <section className={style.sectionContactSection}>
+          <ContactSection />
+        </section>
+      </Layout>
+
+      <Footer />
     </>
   )
 }
